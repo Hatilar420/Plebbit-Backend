94388823d19dd73d2563f5fc0c12da4f1826faa0
Akash <akashgupta515@gmail.com>
2021-05-27 00:46:21 +0530

    Added static

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PostReplyRoutes = require('./Controllers/PostReplyController/PostReplyRout
 const app =  express();
 const port =  8080
 
+app.use(express.static('uploads'))
 app.use(express.json())
 app.use(RegisterLoginRoute)
 app.use("/post",PostRoute)
